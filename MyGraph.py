3978860d4f9d046540802f24f8572369241d3ebe
Chris <christopher_muniz1@baylor.edu>
2023-01-23 13:53:39 -0600

    Hello Test

--- MyGraph.py
+++ MyGraph.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 20, 100)
+x = np.linspace(0, 20, 1002023)
 plt.plot(x, np.sin(x))
 
 plt.show()
@@ -9,3 +9,5 @@ plt.show()
 print("Hello there again!")
 
 print("Hi!")
+
+#pip install matplotlib
